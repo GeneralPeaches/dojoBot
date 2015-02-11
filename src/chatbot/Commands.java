94c18e15e7d9cff81f7c5994cdd98881e6bc7b26
GeneralPeaches <generalpeaches@gmail.com>
2015-02-10 21:31:18 -0500

    Fixed but that crashes bot when command that doesn't exist in database is called

--- src/chatbot/Commands.java
+++ src/chatbot/Commands.java
@@ -351,7 +351,6 @@ public class Commands extends ListenerAdapter {
             connect.close();
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
         }
     }
 
